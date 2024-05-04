54af7926df6d6bda65f9302c2b9aa2e0fee82fad
Diane Corriette <dcorriette@gmail.com>
2024-05-04 16:09:50 +0100

    Create blog using Wagtail. Followed documentation tutorial to create a blog

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ly6c4(zb$#w4fvj%a)i59g8#jls^u!68230o-pv#d%9p(4^#on"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
