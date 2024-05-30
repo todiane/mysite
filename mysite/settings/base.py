6871805b52ba22509b362db5c2321cd364dd23d2
Diane Corriette <dcorriette@gmail.com>
2024-05-30 13:36:51 +0100

    removed Sentry activate code

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+
+
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
--- mysite/urls.py
+++ mysite/urls.py
@@ -9,16 +9,11 @@ from wagtail.documents import urls as wagtaildocs_urls
 from search import views as search_views
 from django.urls import path
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
-    path('sentry-debug/', trigger_error),
 ]
 
 
